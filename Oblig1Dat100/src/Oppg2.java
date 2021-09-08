a10bc38e6952ca36e67a5719629480b2224666f9
pauli <pauli@158.37.224.45>
2021-09-08 14:30:47 +0200

    ferdig

--- Oblig1Dat100/src/Oppg2.java
+++ Oblig1Dat100/src/Oppg2.java
@@ -3,49 +3,42 @@ import javax.swing.JOptionPane;
 public class Oppg2 {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		char karakter = 0;
-		
-		inner:	
-		for (int i=1;i<11;i++) {
-			try {
-				int poengsum =Integer.parseInt(JOptionPane.showInputDialog("Tast inn et poengsum:"));		
-				karakter= Karakter(poengsum);
-			} catch (Exception e) {
+
+		for (int i = 1; i < 11; i++) {
+
+			int poengsum = Integer.parseInt(JOptionPane.showInputDialog("Tast inn et poengsum:"));
+			karakter = Karakter(poengsum);
+			if (karakter != 0) {
+				System.out.println("Elev(" + i + ")" + "sin karakter:" + karakter);
+			} else {
 				i--;
-				continue inner;
+				System.out.println("Ugyldig input!");
 			}
-				System.out.println("Elev("+i+") sin karakter:" + karakter);
 		}
 	}
-	
-	public static boolean gyldigPoengsum(int x) throws Exception {
-		if(x< 0 || x>100) {
-			throw new Exception("Ugyldig poengsum");
+
+	public static char Karakter(int poengsum) throws Exception {
+		char karakter = 0;
+
+		if (poengsum < 40 && poengsum >= 0) {
+			karakter = 'F';
+		} else if (poengsum < 50 && poengsum >= 40) {
+			karakter = 'E';
+		} else if (poengsum < 60 && poengsum >= 50) {
+			karakter = 'D';
+		} else if (poengsum < 80 && poengsum >= 60) {
+			karakter = 'C';
+		} else if (poengsum < 90 && poengsum >= 80) {
+			karakter = 'B';
+		} else if (poengsum <= 100 && poengsum >= 90) {
+			karakter = 'A';
+		} else {
+			karakter = 0;
 		}
-		return true;
-	}
-	public static char Karakter(int poengsum) throws Exception{
-		 char karakter=0;
-		if(gyldigPoengsum(poengsum)) {
-			if(poengsum<40) {
-				karakter ='F';
-			}else if(poengsum<50) {
-				karakter ='E';
-			}else if(poengsum<60) {
-				karakter ='D';
-			}else if(poengsum<80) {
-				karakter ='C';
-			}else if(poengsum<90) {
-				karakter ='B';
-			}else if(poengsum<=100) {
-				karakter ='A';
-			}			
-		}
-		
+
 		return karakter;
+
 	}
 }
-
-
-
